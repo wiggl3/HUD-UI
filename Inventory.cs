b985344b4da90c34d27294c5822b111c66f35c6b
wiggl3 <santana.igor18@gmail.com>
2017-08-24 19:37:31 -0300

    Add files via upload

---
+++ Inventory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour {
+
+	public RectTransform content;
+	public List <GameObject> itens;
+
+	// Use this for initialization
+	void addItem()
+	{
+		while (content.childCount < itens.Count) //com if ele aparece em tempo de jogo, com while ele aparece de uma vez
+		{
+			GameObject item = Instantiate (itens [content.childCount], content.position, Quaternion.identity) as GameObject;
+			item.transform.parent = content.transform;
+		}
+			
+	}
+
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		addItem ();
+	}
+}
